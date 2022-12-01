0368f4b57ab2ddf6f72bd2bbd63687f1faa862f9
Artem <93729278+Nikolaev3Artem@users.noreply.github.com>
2022-12-02 00:49:51 +0200

    some working with angular

--- Angular/angular-website/src/app/app.component.ts
+++ Angular/angular-website/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products'
+import { ProductsService } from './services/products.service';
+import { tap, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-website';
+export class AppComponent implements OnInit {
+  title = 'angular app';
+
+  // products: IProduct[] = []
+  loading = false
+  products$: Observable<IProduct[]> 
+  term = ''
+
+  constructor(private productsService: ProductsService){
+
+  }
+  ngOnInit(): void {
+    this.loading = true
+    this.products$ = this.productsService.getAll().pipe(
+      tap(() => this.loading = false)
+    )
+
+    // this.productsService.getAll().subscribe( products=>{
+    //   this.products = products
+    //   this.loading = false
+    // })
+  }
 }
